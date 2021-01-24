7e08193b5704199504ac798cea076d371228bfc1
Emirhan <Enbiya@LENOVO>
2021-01-24 11:32:23 +0300

    -

---
+++ OOP1/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double Unitprice { get; set; }
+        public int UnıtsInStock { get; set; }
+
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Table";
+            product1.Unitprice = 500;
+            product1.UnıtsInStock = 3;
+
+            //important
+            Product product2 = new Product {Id = 2, CategoryId= 5,
+                UnıtsInStock = 5, ProductName="Pen", Unitprice= 35};
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+            productManager.Topla2(3, 6);
+
+
+            
+            
+                
+
+        }
+    }
+}
